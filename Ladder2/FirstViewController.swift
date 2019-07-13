b8f702c45107716e399cc2cf9c74c314dc6766d6
Kyle Chen <1219887899@qq.com>
2019-07-13 17:24:48 +0800

    Initial Commit

---
+++ Ladder2/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Ladder2
+//
+//  Created by CHENXIN-PC on 2019/7/13.
+//  Copyright © 2019 CHENXIN-PC. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
