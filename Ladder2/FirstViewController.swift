6496f2364ec971294dc1155afe8e45b6f191d8f7
Kyle Chen <1219887899@qq.com>
2019-07-20 18:44:30 +0800

    up

--- Ladder2/FirstViewController.swift
+++ Ladder2/FirstViewController.swift
@@ -8,11 +8,17 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController,BMKMapViewDelegate {
 
+    var _mapView:BMKMapView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        _mapView=BMKMapView(frame:CGRect(x:0,y:45,
+                                         width:self.view.frame.width,
+                                         height:self.view.frame.height))
+        self.view.addSubview(_mapView!)     
     }
 
 
